f154f30bff1e449d91ef38c5f89816c8cd86f3bd
Warrior2852 <95479866+Warrior2852@users.noreply.github.com>
2021-12-07 10:36:06 +0000

    Add files via upload

---
+++ day7p2.py
@@ -0,0 +1,24 @@
+file = open('day7.txt', 'r')
+lines = file.readlines()
+crabs = lines[0].split(',')
+crabs[-1] = crabs[-1].strip('\n')
+for a in range(0, len(crabs)):
+    crabs[a] = int(crabs[a])
+
+currentfuel = 0
+lowestfuel = 0
+for i in crabs:
+    currentfuel = 0
+    complete = 1
+    for j in crabs:
+        if j < i:
+            currentfuel += (sum(range((i - j)+1)))
+        if i < j:
+            currentfuel += (sum(range((j - i)+1)))
+        if currentfuel > lowestfuel and lowestfuel != 0:
+            complete = 0
+            break
+    if complete == 1:
+        lowestfuel = currentfuel
+
+print(lowestfuel)
